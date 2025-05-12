134529f2bbfd1d2e21d80b236d785e8c2b079104
bahodir <vbahodir00@gmail.com>
2025-05-12 19:44:48 +0500

    final correct asgi 2

--- config/asgi.py
+++ config/asgi.py
@@ -1,5 +1,8 @@
 import os
+from django import setup
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
