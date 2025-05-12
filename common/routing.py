2061c9b16f128a399a1e3ee802f7fff1571a5ca3
bahodir <vbahodir00@gmail.com>
2025-05-12 21:43:35 +0500

    correct routing.py

--- common/routing.py
+++ common/routing.py
@@ -1,6 +1,6 @@
-from django.urls import re_path
+from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/counts/$', consumers.CountsConsumer.as_asgi()),
+    path('ws/counts/', consumers.CountsConsumer.as_asgi()),
 ]
